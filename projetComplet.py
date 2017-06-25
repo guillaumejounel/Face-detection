9bdb32098954cfbe7408575c7b91cd897be7cfbd
Guillaume Jounel <guillaume.jounel@etu.utc.fr>
2017-06-25 13:14:52 +0200

    Improved cross validation graph function

--- projetComplet.py
+++ projetComplet.py
@@ -173,7 +173,7 @@ print(" -> Apprentissage...")
 clf.fit(exemples,y)
 
 #Recherche du meilleur C : graphe
-#liblearn.graphValidationCroisee(clf, exemples, 8.5, 9.5, 0.1)
+liblearn.graphValidationCroisee(clf, exemples,y, 0.1, 10, 0.5)
 
 #print(" -> Validation croisée...")
 #print('validation croisée :', liblearn.validationCroisee(clf, exemples, y, 5))
--- pyfacedetect/learn.py
+++ pyfacedetect/learn.py
@@ -18,13 +18,13 @@ def validationCroisee(clf, X, Y, N):
     return np.mean(r)*100
 
 
-def graphValidationCroisee(clf, exemples, debut, fin, pas):
-    choixC = np.zeros((15, 2))
+def graphValidationCroisee(clf, exemples, y, debut, fin, pas):
+    choixC = np.zeros(((fin-debut)//pas+1, 2))
     cursor = 0
     for i in np.arange(debut,fin+pas,pas):
         print(round(100*(i-debut)/(fin-debut)),"¨%")
         clf = svm.SVC(kernel='linear', C=i)
         clf.fit(exemples,y)
-        choixC[cursor] = [i, liblearn.validationCroisee(clf, exemples, y, 5)]
+        choixC[cursor] = [i, validationCroisee(clf, exemples, y, 5)]
         cursor+=1
-    plt.plot(choixC[0:cursor,],choixC[0:cursor,1],'.-')
+    plt.plot(choixC[:,0],choixC[:,1],'.-')
